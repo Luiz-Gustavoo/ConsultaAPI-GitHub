560411f9893a7dd99edccdb566f21190b78f3ec1
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-26 10:11:13 -0300

    commit inicial

---
+++ src/test/java/br/com/pratica/consultaapigithub/ConsultaapigithubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pratica.consultaapigithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaapigithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
