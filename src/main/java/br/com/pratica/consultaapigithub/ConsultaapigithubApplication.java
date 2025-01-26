a31282edc2653a260a0682a1222e8d595f3d2ec3
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-26 10:22:11 -0300

    Adicionar requisição a API do GitHub

--- src/main/java/br/com/pratica/consultaapigithub/ConsultaapigithubApplication.java
+++ src/main/java/br/com/pratica/consultaapigithub/ConsultaapigithubApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @SpringBootApplication
 public class ConsultaapigithubApplication implements CommandLineRunner {
 
@@ -14,5 +19,21 @@ public class ConsultaapigithubApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("teste");
+
+		try{
+			HttpClient httpClient = HttpClient.newHttpClient();
+			HttpRequest httpRequest = HttpRequest.newBuilder()
+					.uri(URI.create("https://api.github.com/users/Luiz-Gustavoo"))
+					.build();
+
+			HttpResponse httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+			System.out.println(httpResponse.body());
+
+		} catch(Exception e) {
+
+		}
 	}
+
+
 }
