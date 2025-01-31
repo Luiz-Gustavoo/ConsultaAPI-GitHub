074f4063da65b9d3cbfbc68fc48215c22be6a4dc
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-31 20:04:20 -0300

    Adicionar método toString

--- src/main/java/br/com/pratica/consultaapigithub/modelos/Usuario.java
+++ src/main/java/br/com/pratica/consultaapigithub/modelos/Usuario.java
@@ -7,4 +7,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public record Usuario(@JsonAlias("login") String nome,
                       @JsonAlias("id") int id,
                       @JsonAlias("html_url") String url_usuario) {
+
+    @Override
+    public String toString() {
+        return
+                "Nome: " + nome + '\n' +
+                "Id: " + id + '\n' +
+                "URL do usuário: " + url_usuario + '\n';
+    }
 }
