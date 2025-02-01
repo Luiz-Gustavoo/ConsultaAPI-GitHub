04658e7a3dd2df5353433ed31e0df24a859df3db
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-31 21:10:04 -0300

    Adicionar loop while, adicionar lista de Usuario, adicionar iteração na lista de Usuario

--- src/main/java/br/com/pratica/consultaapigithub/ConsultaapigithubApplication.java
+++ src/main/java/br/com/pratica/consultaapigithub/ConsultaapigithubApplication.java
@@ -8,11 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ConsultaapigithubApplication implements CommandLineRunner {
@@ -23,20 +20,33 @@ public class ConsultaapigithubApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		List<Usuario> listaUsuarios = new ArrayList<>();
 		ConsumirAPI consumirAPI = new ConsumirAPI();
 		ConverteDados converteDados = new ConverteDados();
 		LerBuscaUsuario lerBuscaUsuario = new LerBuscaUsuario();
+		String buscaUsuario = "";
 
-		String buscaUsuario = lerBuscaUsuario.lerBuscaUsuario();
-
-		//tchurusbangu
-		ResultadoRequisicao respostaAPI = consumirAPI.consumirAPI("https://api.github.com/users/"+buscaUsuario);
-		if (!respostaAPI.isResultado()) {
-			System.out.printf("Não foi possível localizar o usuário '%s' ", buscaUsuario);
-		} else {
-			System.out.println("Dados do usuário buscado: ");
-			Usuario usuarioConvertido = converteDados.converteDados(respostaAPI.getJsonResultado(), Usuario.class);
-			System.out.println(usuarioConvertido);
+		while(!buscaUsuario.equalsIgnoreCase("sair")) {
+			buscaUsuario = lerBuscaUsuario.lerBuscaUsuario();
+			if(buscaUsuario.equalsIgnoreCase("sair")) {
+				break;
+			}
+			//tchurusbangu
+			ResultadoRequisicao respostaAPI = consumirAPI.consumirAPI("https://api.github.com/users/"+buscaUsuario);
+			if (!respostaAPI.isResultado()) {
+				System.out.printf("Não foi possível localizar o usuário '%s'", buscaUsuario);
+			} else {
+				System.out.println("Dados do usuário buscado: ");
+				Usuario usuarioConvertido = converteDados.converteDados(respostaAPI.getJsonResultado(), Usuario.class);
+				listaUsuarios.add(usuarioConvertido);
+				System.out.println(usuarioConvertido);
+			}
 		}
+		System.out.println("-----------------------------------------------");
+		System.out.println("Lista de usuários: ");
+		for (int i = 0; i < listaUsuarios.size(); i++) {
+			System.out.println(listaUsuarios.get(i));
+		}
+		System.out.println("Programa finalizado");
 	}
 }
